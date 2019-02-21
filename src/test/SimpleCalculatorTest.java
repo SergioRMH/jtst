b207f167fbec052718d6627b22eaee0e23e672ff
SergioRMH <sergio.machi@srax.com>
2019-02-21 15:19:32 -0800

    first commit

--- src/test/SimpleCalculatorTest.java
+++ src/test/SimpleCalculatorTest.java
@@ -9,21 +9,21 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.add(1, 1), 4);
 	}
 	@Test
 	public void testSub() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.sub(1, 1), 0);
+		assertEquals(calc.sub(1, 1), 6);
 	}
 	@Test
 	public void testMul() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.mul(3, 5), 15);
+		assertEquals(calc.mul(3, 5), -15);
 	}
 	@Test
 	public void testDiv() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.div(8, 2), 4);
+		assertEquals(calc.div(8, 2), 0);
 	}
 }
